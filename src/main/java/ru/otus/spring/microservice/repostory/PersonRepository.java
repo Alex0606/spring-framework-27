4b11881dabc46fec5cb8f33a589606c696b76710
ydvorzhetskiy <dvorzhetskij@mail.ru>
2018-10-09 22:59:33 +0600

    Final

---
+++ src/main/java/ru/otus/spring/microservice/repostory/PersonRepository.java
@@ -0,0 +1,17 @@
+package ru.otus.spring.microservice.repostory;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import ru.otus.spring.microservice.domain.Person;
+
+import java.util.List;
+
+@RepositoryRestResource(path = "person")
+public interface PersonRepository extends CrudRepository<Person, Integer> {
+
+    List<Person> findAll();
+
+    @RestResource(path = "names")
+    List<Person> findByName(String name);
+}
